a459b9be34f17b0c7a38baec8917a9220e65edab
TeamRory <rorycteam@gmail.com>
2014-06-20 20:12:06 -0400

    Added the reviews model and migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140621000136) do
+ActiveRecord::Schema.define(version: 20140621000958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,14 @@ ActiveRecord::Schema.define(version: 20140621000136) do
     t.integer  "zip_code",                 null: false
     t.string   "description", limit: 1000
     t.string   "category"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "reviews", force: true do |t|
+    t.integer  "rating",        null: false
+    t.string   "body",          null: false
+    t.integer  "restaurant_id", null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
