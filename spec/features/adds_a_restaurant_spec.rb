ee24a1110f984721cd4152a23088e6ca5805be3a
TeamRory <rorycteam@gmail.com>
2014-06-21 09:22:27 -0400

    Added two new tests added the two tests to verify no imput and duplicate input for restaurant form

--- spec/features/adds_a_restaurant_spec.rb
+++ spec/features/adds_a_restaurant_spec.rb
@@ -1,6 +1,6 @@
 require 'spec_helper'
 
-feature "User adds a new restaurant" %Q{
+feature "User adds a new restaurant", %Q{
   As a user I want to be able to add a new
   restaurant to let other people know about it
   and review it.
@@ -36,7 +36,39 @@ feature "User adds a new restaurant" %Q{
     expect(page).to have_content restaurant.zip_code
   end
 
-  scenario 'without required attributes'
+  scenario 'without required attributes' do
+    visit '/restaurants/new'
+    click_on 'Submit'
 
+    expect(page).to_not have_content 'Success'
+    expect(page).to have_content "can't be blank"
+    expect(page).to_not have_content restaurant.name
+  end
 
+  scenario 'user tries to add a duplicate restaurant' do
+    attrs = {
+      name: 'Lotus Thai Bistro',
+      address: '3019 Carlsbad village dr.',
+      city: 'Carlsbad',
+      state: 'California',
+      zip_code: 92008,
+      description: 'Great Thai food',
+      category: 'Thai'
+    }
+
+    restaurant = Restaurant.create(attrs)
+
+    visit '/restaurants/new'
+    fill_in 'Name', with: restaurant.name
+    fill_in 'Address', with: restaurant.address
+    fill_in 'City', with: restaurant.city
+    fill_in 'State', with: restaurant.state
+    fill_in 'Zip code', with: restaurant.zip_code
+    fill_in 'Description', with: restaurant.description
+    fill_in 'category', with: restaurant.category
+    click_on 'Submit'
+
+    expect(page).to have_content 'has already been taken'
+    expect(page).to_not have_content 'Success'
+  end
 end
