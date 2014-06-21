a37ceaaf8316a797fd03b9723309a545a3c9ecdb
TeamRory <rorycteam@gmail.com>
2014-06-21 12:42:47 -0400

    Got my second test to pass. The restaurants show page is now functional along with the controller

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,5 +1,10 @@
 class RestaurantsController < ApplicationController
+
   def index
     @restaurants = Restaurant.order('created_at DESC')
+  end
+
+  def show
+    @restaurant = Restaurant.find(params[:id])
   end
 end
