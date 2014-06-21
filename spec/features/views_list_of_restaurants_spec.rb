990223dee110907736db44b560b77ed902f0c5b0
TeamRory <rorycteam@gmail.com>
2014-06-20 20:57:20 -0400

    Added my first test. Tests the restaurant index page

---
+++ spec/features/views_list_of_restaurants_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+  feature "User views a list of restaurants", %Q{
+    As a user I would like to navigate to a page
+    and be able to see a list of restaurants
+    names and addresses to find one close to me.
+  } do
+
+    scenario 'User views a list of restaurants' do
+      restaurants = []
+      restaurant_attrs = [
+        {name: 'Lotus Thai Bistro', address: '3019 Carlsbad village dr.', city: 'Carlsbad', state: 'California', zip_code: 92008, description: 'Great Thai food', category: 'Thai'},
+        {name: 'The Compass', address: '123 Carlsbad blvd.', city: 'Carlsbad', state: 'California', zip_code: 92008, description: 'Old fashiond bar/restaurant', category: 'Bar'}
+      ]
+      restaurant_attrs.each do |attrs|
+        restaurants << Restaurant.create(attrs)
+      end
+
+      visit '/restaurants'
+      restaurants.each do |restaurant|
+      expect(page).to have_content restaurant.name
+      expect(page).to have_content restaurant.address
+    end
+  end
+end
