fee20096d473b2124a3119be33f6603509b40dbd
TeamRory <rorycteam@gmail.com>
2014-06-22 15:51:48 -0400

    Added some links and a method. links to navigate around easily and the method to calculate the restaurants average rating

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -6,4 +6,21 @@ class Restaurant < ActiveRecord::Base
   validates :city, presence: true
   validates :state, presence: true
   validates :zip_code, presence: true
+
+  def average_rating
+    count = 0
+    total = 0
+    average_rating =
+    self.reviews.each do |review|
+      total = total + review.rating.to_f
+      count +=1
+    end
+    if count == 0
+      average_rating = 'N/A'
+    else
+      average_rating = total/count
+      average_rating = average_rating.round(2)
+    end
+    average_rating
+  end
 end
