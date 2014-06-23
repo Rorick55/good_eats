6f9932c528b274b808007e64667ff284fb6b99e3
TeamRory <rorycteam@gmail.com>
2014-06-22 20:31:59 -0400

    I forgot to limit the restaurants page. now only the most recent 10 restaurants can be seen

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,7 +1,7 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurants = Restaurant.order('created_at DESC')
+    @restaurants = Restaurant.order('created_at DESC').limit('10')
   end
 
   def show
